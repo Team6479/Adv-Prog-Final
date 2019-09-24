c1d88092b43c7110ee9d1cebecf664f928f51e8f
2stinkysocks <zmehall@gmail.com>
2019-09-24 16:12:35 -0700

    Done?

--- src/main/java/frc/robot/commands/SpinButton.java
+++ src/main/java/frc/robot/commands/SpinButton.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class SpinButton extends Command {
-  
+   
   private boolean finished = false;
 
   public SpinButton() {
